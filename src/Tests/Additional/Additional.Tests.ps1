2ec0b2dd809b46a0becd3424bf1bc97509c88474
Can Liu <can.liu5@patchmypc.com>
2026-05-13 09:06:11 +0800

    Add test case

---
+++ src/Tests/Additional/Additional.Tests.ps1
@@ -0,0 +1,15 @@
+Describe 'Additional Tests' {
+    Context 'Sanity checks' {
+        It 'PowerShell version is 5.1 or higher' {
+            $PSVersionTable.PSVersion.Major | Should -BeGreaterOrEqual 5
+        }
+
+        It 'True is true' {
+            $true | Should -BeTrue
+        }
+
+        It 'Basic arithmetic works' {
+            (1 + 1) | Should -Be 2
+        }
+    }
+}
