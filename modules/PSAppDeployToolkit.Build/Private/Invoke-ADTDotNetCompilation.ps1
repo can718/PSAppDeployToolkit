6ca7d4ee1fa656f23e0f3b8c3cc470fb30788dc7
Can Liu <can.liu5@patchmypc.com>
2026-09-18 14:54:46 +0800

    remove debug information

--- modules/PSAppDeployToolkit.Build/Private/Invoke-ADTDotNetCompilation.ps1
+++ modules/PSAppDeployToolkit.Build/Private/Invoke-ADTDotNetCompilation.ps1
@@ -151,18 +151,6 @@ function Invoke-ADTDotNetCompilation
                 try
                 {
                     $testArguments = [System.Collections.Generic.List[System.String]]@('test', '--solution', $buildItem.SolutionPath, '--configuration', $buildType, '--no-build', '--no-restore', '--report-trx')
-                    # TEMPORARY: PSADT-HANG-DIAGNOSTICS - Remove after the intermittent self-hosted runner hang is diagnosed.
-                    if (![System.String]::IsNullOrWhiteSpace($env:PSADT_TEST_HANG_TIMEOUT))
-                    {
-                        $hangDumpDirectory = [System.IO.Directory]::CreateDirectory((Join-Path $Script:ModuleConstants.Paths.Repository 'TestResults\HangDumps')).FullName
-                        $testArguments.Add('--hangdump')
-                        $testArguments.Add('--hangdump-timeout')
-                        $testArguments.Add($env:PSADT_TEST_HANG_TIMEOUT)
-                        $testArguments.Add('--hangdump-type')
-                        $testArguments.Add('Mini')
-                        $testArguments.Add('--hangdump-filename')
-                        $testArguments.Add((Join-Path $hangDumpDirectory '{asm}-{tfm}-{pid}-{time}.dmp'))
-                    }
                     & $dotnet $testArguments | Write-ADTDotNetOutputBuildLogEntry
                     if ($Global:LASTEXITCODE)
                     {
